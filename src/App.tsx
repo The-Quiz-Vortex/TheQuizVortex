f762c58f7f248901ad533fb037caaac1bcedbafe
R@d0 <rkolev95@gmail.com>
2023-11-23 01:26:05 +0200

    Install ChakraUI; Update Header view

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,38 @@
-import './App.css'
+import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './views/Home/Home.tsx';
 import Signin from './components/Signin/Signin.tsx';
 import Signup from './components/Signup/Signup.tsx';
+import Header from './components/Header/Header.tsx';
 import { AuthContext } from './context/AuthContext.tsx';
 import { useUserContext } from './helpers/useUserContext.ts';
 import _ from 'lodash';
 
+import * as React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+
 function App() {
-  const {
-    loading,
-    user,
-    appState,
-    setAppState } = useUserContext()
+  const { loading, user, appState, setAppState } = useUserContext();
 
   return (
-    <>
-      {((!loading && user && !_.isEmpty(appState.userData)) || (!loading && !user)) &&
-        (<AuthContext.Provider value={{ ...appState, setUser: setAppState }}>
-          <div>
-            <Routes>
-              <Route path='/home' element={<Home />} />
-              <Route path='/' element={<Home />} />
-              <Route path='/sign-in' element={<Signin />} />
-              <Route path='/sign-up' element={<Signup />} />
-            </Routes>
-          </div>
-        </AuthContext.Provider>)}
-    </>
-  )
+    <ChakraProvider>
+      <div>
+        {((!loading && user && !_.isEmpty(appState.userData)) || (!loading && !user)) && (
+          <AuthContext.Provider value={{ ...appState, setUser: setAppState }}>
+            <div>
+              <Header />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/sign-in" element={<Signin />} />
+                <Route path="/sign-up" element={<Signup />} />
+              </Routes>
+            </div>
+          </AuthContext.Provider>
+        )}
+      </div>
+    </ChakraProvider>
+  );
 }
 
-export default App
+export default App;
