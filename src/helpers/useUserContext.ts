304bdc97673b3990151ee81ed55025c0a4d27f1b
R@d0 <rkolev95@gmail.com>
2023-11-28 01:01:36 +0200

    Fix MyProfile; Add validation; Create getUserByUID

--- src/helpers/useUserContext.ts
+++ src/helpers/useUserContext.ts
@@ -1,16 +1,16 @@
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth, db } from "../config/firebase-config.ts";
-import { useEffect, useState } from "react";
-import { getUserByEmail } from "../services/users.services.ts";
-import { onValue, ref } from "firebase/database";
-import { AppUser } from "../common/interfaces.ts";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db } from '../config/firebase-config.ts';
+import { useEffect, useState } from 'react';
+import { getUserByEmail, getUserByUID } from '../services/users.services.ts';
+import { onValue, ref } from 'firebase/database';
+import { AppUser } from '../common/interfaces.ts';
 import { User, UserCredential } from 'firebase/auth';
 
 export interface AppState {
-    user: User | UserCredential | null | undefined;
-    userData?: AppUser | null;
-    isLoggedIn?: boolean;
-  }
+  user: User | UserCredential | null | undefined;
+  userData?: AppUser | null;
+  isLoggedIn?: boolean;
+}
 
 export const useUserContext = () => {
   const [user, loading] = useAuthState(auth);
@@ -21,7 +21,7 @@ export const useUserContext = () => {
   });
 
   if (appState.user !== user) {
-    setAppState(prevState => ({ ...prevState, user }));
+    setAppState((prevState) => ({ ...prevState, user }));
   }
 
   useEffect(() => {
@@ -33,20 +33,22 @@ export const useUserContext = () => {
       });
       return;
     }
-    !loading && (async () => {
-      try {
-        if (user?.email) {
-            const result = await getUserByEmail(user.email);
+    !loading &&
+      (async () => {
+        try {
+          if (user?.email) {
+            const result = await getUserByUID(user.uid);
+
             setAppState((prev) => ({
               ...prev,
               userData: Object.values(result.val())[0] as AppUser,
               isLoggedIn: !!result,
             }));
+          }
+        } catch (error) {
+          console.log(error.message);
         }
-      } catch (error) {
-        console.log(error.message);
-      }
-    })();
+      })();
   }, [user, loading, appState.userData?.profilePictureURL]);
 
   useEffect(() => {
@@ -54,7 +56,7 @@ export const useUserContext = () => {
       const userRef = ref(db, `users/${appState.userData.username}`);
 
       const userListener = onValue(userRef, (snapshot) => {
-        setAppState((prev) => ({ ...prev, userData: snapshot.val() }))
+        setAppState((prev) => ({ ...prev, userData: snapshot.val() }));
       });
       return () => {
         userListener();
@@ -66,6 +68,6 @@ export const useUserContext = () => {
     loading,
     user,
     appState,
-    setAppState
-  }
-}
+    setAppState,
+  };
+};
--- src/services/users.services.ts
+++ src/services/users.services.ts
@@ -1,6 +1,6 @@
-import { get, set, ref, query, equalTo, orderByChild, update, remove  } from 'firebase/database';
-import { db } from "../config/firebase-config.ts";
-import { DataSnapshot } from "firebase/database";
+import { get, set, ref, query, equalTo, orderByChild, update, remove } from 'firebase/database';
+import { db } from '../config/firebase-config.ts';
+import { DataSnapshot } from 'firebase/database';
 import { setFileToFirebaseStorage } from './storage.services.ts';
 
 /**
@@ -8,17 +8,17 @@ import { setFileToFirebaseStorage } from './storage.services.ts';
  *
  */
 export const fromUsersDocument = (snapshot: DataSnapshot) => {
-    const usersDocument = snapshot.val();
+  const usersDocument = snapshot.val();
 
-    return Object.keys(usersDocument).map((key) => {
-        const post = usersDocument[key];
+  return Object.keys(usersDocument).map((key) => {
+    const post = usersDocument[key];
 
-        return {
-            ...post,
-            username: key,
-            createdOn: new Date(post.createdOn),
-        };
-    });
+    return {
+      ...post,
+      username: key,
+      createdOn: new Date(post.createdOn),
+    };
+  });
 };
 
 /**
@@ -28,7 +28,11 @@ export const fromUsersDocument = (snapshot: DataSnapshot) => {
  * @returns {Promise<Object>} - A promise that resolves with the retrieved user object.
  */
 export const getUserByUsername = async (username: string): Promise<DataSnapshot> => {
-    return get(ref(db, `users/${username}`));
+  return get(ref(db, `users/${username}`));
+};
+
+export const getUserByUID = (uid: string): Promise<DataSnapshot> => {
+  return get(query(ref(db, 'users'), orderByChild('uid'), equalTo(uid)));
 };
 
 /**
@@ -39,9 +43,19 @@ export const getUserByUsername = async (username: string): Promise<DataSnapshot>
  * or undefined if no user exists with that email.
  */
 export const getUserByEmail = (email: string): Promise<DataSnapshot> => {
-    return get(query(ref(db, 'users'), orderByChild('email'), equalTo(email)));
+  return get(query(ref(db, 'users'), orderByChild('email'), equalTo(email)));
 };
 
+/**
+ * Gets user details by phone number.
+ * : Promise<object>
+ * @param {string} phoneNumber - The phone number to search for.
+ * @returns {Promise<Object>} A promise that resolves to the User object if found,
+ * or undefined if no user exists with that phone number.
+ */
+export const getUserByPhone = (phoneNumber: string): Promise<DataSnapshot> => {
+  return get(query(ref(db, 'users'), orderByChild('phoneNumber'), equalTo(phoneNumber)));
+};
 
 /**
  * Creates a new user using their username as the key.
@@ -55,25 +69,24 @@ export const getUserByEmail = (email: string): Promise<DataSnapshot> => {
  * @returns {Promise<void>} - A promise that resolves after creating the user.
  */
 export const createUserByUsername = (
-    firstName: string,
-    lastName: string,
-    uid: string,
-    email: string,
-    phoneNumber: string,
-    username: string,
+  firstName: string,
+  lastName: string,
+  uid: string,
+  email: string,
+  phoneNumber: string,
+  username: string
 ): Promise<void> => {
-    return set(ref(db, `users/${username}`), {
-        firstName,
-        lastName,
-        uid,
-        username,
-        email,
-        phoneNumber,
-        role: "user",
-        createdOn: Date.now(),
-    });
+  return set(ref(db, `users/${username}`), {
+    firstName,
+    lastName,
+    uid,
+    username,
+    email,
+    phoneNumber,
+    role: 'user',
+    createdOn: Date.now(),
+  });
 };
-
 
 /**
  * Retrieves user data by UID.
@@ -82,20 +95,20 @@ export const createUserByUsername = (
  * @returns {Promise<Object>} - A promise that resolves with the retrieved user object.
  */
 export const getUserData = (uid: string): Promise<DataSnapshot> => {
-    return get(query(ref(db, 'users'), orderByChild('uid'), equalTo(uid)));
+  return get(query(ref(db, 'users'), orderByChild('uid'), equalTo(uid)));
 };
 
 /**
  * Retrieves all users.
  */
 export const getAllUsers = () => {
-    return get(ref(db, "users")).then((snapshot) => {
-        if (!snapshot.exists()) {
-            return [];
-        }
+  return get(ref(db, 'users')).then((snapshot) => {
+    if (!snapshot.exists()) {
+      return [];
+    }
 
-        return fromUsersDocument(snapshot);
-    });
+    return fromUsersDocument(snapshot);
+  });
 };
 
 /**
@@ -105,18 +118,14 @@ export const getAllUsers = () => {
  * @param {string} currentUser - The username of the current user.
  * @returns {Promise<string>} - A promise that resolves with the created profile picture URL.
  */
-export const createProfilePic = async (
-    file: File,
-    currentUser: string
-  ): Promise<string> => {
-    const url = await setFileToFirebaseStorage(file);
-  
-    // Use set to create a new profile picture URL
-    await set(ref(db, `/users/${currentUser}/profilePictureURL`), url);
-  
-    return url;
-  };
-  
+export const createProfilePic = async (file: File, currentUser: string): Promise<string> => {
+  const url = await setFileToFirebaseStorage(file);
+
+  // Use set to create a new profile picture URL
+  await set(ref(db, `/users/${currentUser}/profilePictureURL`), url);
+
+  return url;
+};
 
 /**
  * Updates the profile picture URL for a user.
@@ -125,19 +134,15 @@ export const createProfilePic = async (
  * @param {string} currentUser - The username of the current user.
  * @returns {Promise<string>} - A promise that resolves with the updated profile picture URL.
  */
-export const updateProfilePic = async (
-    file: File,
-    currentUser: string
-): Promise<string> => {
-    const url = await setFileToFirebaseStorage(file);
+export const updateProfilePic = async (file: File, currentUser: string): Promise<string> => {
+  const url = await setFileToFirebaseStorage(file);
 
-    const updateProfilePic: { [key: string]: string } = {};
-    updateProfilePic[`/users/${currentUser}/profilePictureURL`] = url;
+  const updateProfilePic: { [key: string]: string } = {};
+  updateProfilePic[`/users/${currentUser}/profilePictureURL`] = url;
 
-    update(ref(db), updateProfilePic);
-    return url;
+  update(ref(db), updateProfilePic);
+  return url;
 };
-
 
 /**
  * Deletes the profile picture URL for a user.
@@ -146,16 +151,15 @@ export const updateProfilePic = async (
  * @returns {Promise<void>} - A promise that resolves when the profile picture URL is deleted.
  */
 export const deleteProfilePic = async (currentUser: string): Promise<void> => {
-    const path = `/users/${currentUser}/profilePictureURL`;
-  
-    try {
-      await remove(ref(db, path));
-    } catch (error) {
-      console.error('Error deleting profile picture URL:', error);
-      throw new Error('Failed to delete profile picture URL');
-    }
-  };
+  const path = `/users/${currentUser}/profilePictureURL`;
 
+  try {
+    await remove(ref(db, path));
+  } catch (error) {
+    console.error('Error deleting profile picture URL:', error);
+    throw new Error('Failed to delete profile picture URL');
+  }
+};
 
 /**
  * Updates the first name for a user.
@@ -165,15 +169,14 @@ export const deleteProfilePic = async (currentUser: string): Promise<void> => {
  * @returns {Promise<void>} - A promise that resolves after updating the phone number.
  */
 export const updateProfileFirstName = async (
-    firstName: string,
-    currentUser: string
+  firstName: string,
+  currentUser: string
 ): Promise<void> => {
-    const updateFirstName: { [key: string]: string } = {};
-    updateFirstName[`/users/${currentUser}/firstName`] = firstName;
+  const updateFirstName: { [key: string]: string } = {};
+  updateFirstName[`/users/${currentUser}/firstName`] = firstName;
 
-    return update(ref(db), updateFirstName);
+  return update(ref(db), updateFirstName);
 };
-
 
 /**
  * Updates the last name for a user.
@@ -183,16 +186,14 @@ export const updateProfileFirstName = async (
  * @returns {Promise<void>} - A promise that resolves after updating the phone number.
  */
 export const updateProfileLastName = async (
-    lastName: string,
-    currentUser: string
+  lastName: string,
+  currentUser: string
 ): Promise<void> => {
-    const updateLastName: { [key: string]: string } = {};
-    updateLastName[`/users/${currentUser}/lastName`] = lastName;
+  const updateLastName: { [key: string]: string } = {};
+  updateLastName[`/users/${currentUser}/lastName`] = lastName;
 
-    return update(ref(db), updateLastName);
+  return update(ref(db), updateLastName);
 };
-
-
 
 /**
  * Updates the phone number for a user.
@@ -202,16 +203,14 @@ export const updateProfileLastName = async (
  * @returns {Promise<void>} - A promise that resolves after updating the phone number.
  */
 export const updateProfilePhone = async (
-    phoneNumber: string,
-    currentUser: string
+  phoneNumber: string,
+  currentUser: string
 ): Promise<void> => {
-    const updatePhone: { [key: string]: string } = {};
-    updatePhone[`/users/${currentUser}/phoneNumber`] = phoneNumber;
+  const updatePhone: { [key: string]: string } = {};
+  updatePhone[`/users/${currentUser}/phoneNumber`] = phoneNumber;
 
-    return update(ref(db), updatePhone);
+  return update(ref(db), updatePhone);
 };
-
-
 
 /**
  * Updates the email address for a user.
@@ -220,16 +219,12 @@ export const updateProfilePhone = async (
  * @param {string} currentUser - The username of the current user.
  * @returns {Promise<void>} - A promise that resolves after updating the email.
  */
-export const updateProfileEmail = async (
-    email: string,
-    currentUser: string
-): Promise<void> => {
-    const updateEmail: { [key: string]: string } = {};
-    updateEmail[`/users/${currentUser}/email`] = email;
+export const updateProfileEmail = async (email: string, currentUser: string): Promise<void> => {
+  const updateEmail: { [key: string]: string } = {};
+  updateEmail[`/users/${currentUser}/email`] = email;
 
-    return update(ref(db), updateEmail);
+  return update(ref(db), updateEmail);
 };
-
 
 /**
  * Blocks a user.
@@ -238,13 +233,12 @@ export const updateProfileEmail = async (
  * @returns {Promise<void>} - A promise that resolves after blocking the user.
  */
 export const blockUser = (username: string): Promise<void> => {
-    const updateBlockedStatus: { [key: string]: boolean } = {};
+  const updateBlockedStatus: { [key: string]: boolean } = {};
 
-    updateBlockedStatus[`/users/${username}/blockedStatus`] = true;
+  updateBlockedStatus[`/users/${username}/blockedStatus`] = true;
 
-    return update(ref(db), updateBlockedStatus);
+  return update(ref(db), updateBlockedStatus);
 };
-
 
 /**
  * Unblocks a user.
@@ -253,13 +247,12 @@ export const blockUser = (username: string): Promise<void> => {
  * @returns {Promise<void>} - A promise that resolves after unblocking the user.
  */
 export const unblockUser = (username: string): Promise<void> => {
-    const updateBlockedStatus: { [key: string]: boolean } = {};
+  const updateBlockedStatus: { [key: string]: boolean } = {};
 
-    updateBlockedStatus[`/users/${username}/blockedStatus`] = false;
+  updateBlockedStatus[`/users/${username}/blockedStatus`] = false;
 
-    return update(ref(db), updateBlockedStatus);
+  return update(ref(db), updateBlockedStatus);
 };
-
 
 /**
  * Grants admin privileges to a user.
@@ -269,13 +262,12 @@ export const unblockUser = (username: string): Promise<void> => {
  */
 
 export const makeAdminUser = (username: string): Promise<void> => {
-    const updateAdminStatus: { [key: string]: string } = {};
+  const updateAdminStatus: { [key: string]: string } = {};
 
-    updateAdminStatus[`/users/${username}/role`] = "admin";
+  updateAdminStatus[`/users/${username}/role`] = 'admin';
 
-    return update(ref(db), updateAdminStatus);
+  return update(ref(db), updateAdminStatus);
 };
-
 
 /**
  * Removes admin privileges from a user.
@@ -284,20 +276,9 @@ export const makeAdminUser = (username: string): Promise<void> => {
  * @returns {Promise<void>} - A promise that resolves after removing admin privileges.
  */
 export const removeAdminUser = (username: string): Promise<void> => {
-    const updateAdminStatus: { [key: string]: string } = {};
+  const updateAdminStatus: { [key: string]: string } = {};
 
-    updateAdminStatus[`/users/${username}/role`] = "user";
+  updateAdminStatus[`/users/${username}/role`] = 'user';
 
-    return update(ref(db), updateAdminStatus);
+  return update(ref(db), updateAdminStatus);
 };
-
-
-
-
-
-
-
-
-
-
-
