96230d0e275cc7a99461597f325ef45807a0bea8
ventsislav ivanov <venci77xxx@gmail.com>
2023-11-28 09:04:47 +0200

    Add user authentication functionality

--- src/helpers/useUserContext.ts
+++ src/helpers/useUserContext.ts
@@ -10,6 +10,7 @@ export interface AppState {
   user: User | UserCredential | null | undefined;
   userData?: AppUser | null;
   isLoggedIn?: boolean;
+  isAdmin?: boolean;
 }
 
 export const useUserContext = () => {
@@ -33,38 +34,25 @@ export const useUserContext = () => {
       });
       return;
     }
+
     !loading &&
       (async () => {
         try {
           if (user?.email) {
             const result = await getUserByUID(user.uid);
+            const isAdmin = result && result.val()?.role === 'admin';
 
             setAppState((prev) => ({
               ...prev,
               userData: Object.values(result.val())[0] as AppUser,
               isLoggedIn: !!result,
+              isAdmin,
             }));
           }
         } catch (error) {
           console.log(error.message);
         }
       })();
-    !loading && (async () => {
-      try {
-        if (user?.email) {
-          const result = await getUserByEmail(user.email);
-          const isAdmin = result && result.val()?.role === 'admin';
-          setAppState((prev) => ({
-            ...prev,
-            userData: Object.values(result.val())[0] as AppUser,
-            isLoggedIn: !!result,
-            isAdmin,
-          }));
-        }
-      } catch (error) {
-        console.log(error.message);
-      }
-    })();
   }, [user, loading, appState.userData?.profilePictureURL]);
 
   useEffect(() => {
