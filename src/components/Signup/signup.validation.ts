19d5446eb22634df61c6821f38a1f48634b13a8b
tihomir.nikolov1988 <tihomir.nikolov1988@gmail.com>
2023-12-03 16:38:36 +0200

    Fix name validation

--- src/components/Signup/signup.validation.ts
+++ src/components/Signup/signup.validation.ts
@@ -2,14 +2,20 @@ import { getUserByEmail, getUserByUsername } from "../../services/users.services
 import * as z from "zod"
 
 export const SignupValidation = z.object({
-    firstName: z.string().min(1, { message: 'Min length should be 1 symbols' }).max(30, { message: 'Max length should be 30 symbols' }),
-    lastName: z.string().min(1, { message: 'Min length should be 1 symbols' }).max(30, { message: 'Max length should be 30 symbols' }),
+    firstName: z.string()
+        .min(1, { message: 'Min length should be 1 symbols' })
+        .max(30, { message: 'Max length should be 30 symbols' })
+        .regex(new RegExp(/^[a-zA-Z]+[-'s]?[a-zA-Z ]+$/), 'Only letters allowed'),
+    lastName: z.string()
+        .min(1, { message: 'Min length should be 1 symbols' })
+        .max(30, { message: 'Max length should be 30 symbols' })
+        .regex(new RegExp(/^[a-zA-Z]+[-'s]?[a-zA-Z ]+$/), 'Only letters allowed'),
     username: z.string().min(3, { message: 'Min length should be 3 symbols' })
         .refine(async (value) => !(await getUserByUsername(value)).val(), { message: 'This username already exists' }),
     email: z.string().email().refine(async (email) => {
         return !((await getUserByEmail(email)).val())
     }, { message: 'Email already exists' },),
-    phoneNumber: z.string().length(10, { message: 'The phone number should be 10 digits' }),
+    phoneNumber: z.string().length(10, { message: '10 digits required' }),
     password: z
         .string()
         .min(6, { message: "Password must be at least 6 characters" }),
