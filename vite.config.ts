dc51178c682562f80b7beafa548638332e5ee02b
tihomir.nikolov1988 <tihomir.nikolov1988@gmail.com>
2024-01-29 20:06:38 +0200

    Fix scss and host app

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import sass from 'vite-plugin-sass';
+import sassGlobImports from 'vite-plugin-sass-glob-import';
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd())
 // https://vitejs.dev/config/
 return defineConfig({
-  plugins: [react(), sass()],
+  plugins: [
+    sassGlobImports(),
+    react(),
+  ],
   define: {
     'process.env': env
   }
